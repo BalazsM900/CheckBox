071799f9839edec4e6727df49c8e813ffa684958
Molnár Balázs <molnarbalazs1226@gmail.com>
2024-09-12 13:45:10 +0200

    asd

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    const l = Array(50).fill(0)
+</script>
+<div class=g>
+{#each l as _}
+    <button class=l{_} 
+            on:click={() => 
+                _ = _ == 1 ? 0 : 1
+            }
+    ></button>
+{/each}
+</div>
+<style>
+.g {
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+}
+.g button {
+    padding:10px;
+    border: 1px solid black;
+    text-align: center;
+    cursor: pointer;
+}
+.g button.l0 {
+    background-color: aqua;
+}
+.g button.l1 {
+    background-color: red;
+}
+</style>
